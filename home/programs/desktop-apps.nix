22583ddec54ac88768b41f275fbdb6906394ad3e
Theneillsaaco <isaacdepena18@gmail.com>
2026-03-28 19:31:13 -0400

    Update flake inputs, pin Discord, disable Hyprland

--- home/programs/desktop-apps.nix
+++ home/programs/desktop-apps.nix
@@ -1,6 +1,12 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    discord
+    (discord.overrideAttrs (old: {
+      version = "0.0.130";
+      src = pkgs.fetchurl {
+        url = "https://dl.discordapp.net/apps/linux/0.0.130/discord-0.0.130.tar.gz";
+        sha256 = "sha256-FMIM/CWPk3Kcqp8iIg+gxiCpjD2DNk7dSBqnCBvzn5w=";
+      };
+    }))
     brave
     steam
     pear-desktop

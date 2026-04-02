b166984722e976744101ad3db5b0512d5e2e70db
Theneillsaaco <isaacdepena18@gmail.com>
2026-04-02 17:02:14 -0400

    Enable firewall with specific ports
    Enable the firewall and allow incoming TCP traffic on ports 4321 and
    54321.

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -1,6 +1,11 @@
 {
   networking.hostName = "nixos"; # Define your hostname.
   networking.networkmanager.enable = true;
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 4321 54321 ];
+  };
   
   services.resolved.enable = true;
 }

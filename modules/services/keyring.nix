10c5f7c140b4ce82055abb69f53af8d1215feb3f
Theneillsaaco <isaacdepena18@gmail.com>
2026-04-08 16:54:48 -0400

    Update lock file and enable KWallet for SDDM
    Update flake.lock to reflect recent changes in dependencies.
    Additionally, enable KWallet support for the SDDM login manager.

--- modules/services/keyring.nix
+++ modules/services/keyring.nix
@@ -3,6 +3,9 @@
   
   security.pam.services.isaac.kwallet = {
     enable = true;
+    enableKwallet = true;
     package = pkgs.kdePackages.kwallet-pam;
   };
+  
+  security.pam.services.sddm.enableKwallet = true;
 }

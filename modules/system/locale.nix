1f4e0a120d5e43bfca07da7ba8d9d1a430f85432
Theneillsaaco <isaacdepena18@gmail.com>
2026-04-16 22:04:36 -0400

    Add extensive starship configuration
    Moves starship configuration from a dedicated file into the main
    configuration.
    This commit includes a significant number of customizations for the
    starship
    prompt, enhancing its appearance and functionality. New features
    include:
    detailed git status indicators, enhanced directory display, battery
    information, and support for various programming languages and
    environments.
    Additionally, locale settings have been expanded to include more
    options.

--- modules/system/locale.nix
+++ modules/system/locale.nix
@@ -3,10 +3,22 @@
 
   i18n.defaultLocale = "en_US.UTF-8";
 
+  i18n.supportedLocales = [ 
+    "en_US.UTF-8/UTF-8"
+    "nl_NL.UTF-8/UTF-8"
+    "nl_NL/ISO-8859-1"
+    "es_DO.UTF-8/UTF-8"
+  ];
+  
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "es_DO.UTF-8";
+    LC_IDENTIFICATION = "es_DO.UTF-8";
     LC_MEASUREMENT = "es_DO.UTF-8";
     LC_MONETARY = "es_DO.UTF-8";
+    LC_NAME = "es_DO.UTF-8";
+    LC_NUMERIC = "es_DO.UTF-8";
+    LC_PAPER = "es_DO.UTF-8";
+    LC_TELEPHONE = "es_DO.UTF-8";    
     LC_TIME = "es_DO.UTF-8";
   };
 }

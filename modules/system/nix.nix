86c44ff8b914a6413c75ffcd68d565d099d8956d
Theneillsaaco <isaacdepena18@gmail.com>
2026-04-18 15:40:07 -0400

    Add theneilsaaco-nix cachix.org cache

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -21,10 +21,12 @@
       substituters = [
         "https://cache.nixos.org/"
         "https://nix-community.cachix.org"
+        "https://theneillsaaco-nix.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "theneillsaaco-nix.cachix.org-1:l6861n9yzzvrcRmsa8xJuF2abe8R+7j++fz3j1C1/I4="
       ];
       
       trusted-users = [ "root" "alice" "@wheel" ];

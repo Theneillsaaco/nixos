27cab1c2e7322fe6c253a1d860f24e80d6675b46
Theneillsaaco <isaacdepena18@gmail.com>
2026-04-19 16:12:13 -0400

    Fix typo in README installation instructions

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -25,16 +25,16 @@
       lazy-trees = true;
       
       substituters = [
-        "https://theneillsaaco-nix.cachix.org"
-        "https://hyprland.cachix.org"
         "https://nix-community.cachix.org"
         "https://cache.nixos.org/"
+        "https://theneillsaaco-nix.cachix.org"
+        "https://hyprland.cachix.org"
       ];
       trusted-public-keys = [
-        "theneillsaaco-nix.cachix.org-1:l6861n9yzzvrcRmsa8xJuF2abe8R+7j++fz3j1C1/I4="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "theneillsaaco-nix.cachix.org-1:l6861n9yzzvrcRmsa8xJuF2abe8R+7j++fz3j1C1/I4="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
       
       trusted-users = [ "root" "@wheel" ];

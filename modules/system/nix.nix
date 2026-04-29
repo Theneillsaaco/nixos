db64e67d72499fba019c9ff52213c127d21c852d
Theneillsaaco <isaacdepena18@gmail.com>
2026-04-29 18:08:36 -0400

    Add Determinate Systems cache

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -27,12 +27,14 @@
       
       substituters = [
         "https://nix-community.cachix.org"
+        "https://cache.determinate.systems"
         "https://cache.nixos.org/"
         "https://theneillsaaco-nix.cachix.org"
         "https://hyprland.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.determinate.systems-1:wup8m09p2798f09p2798f09p2798f09p2798f09p279="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "theneillsaaco-nix.cachix.org-1:l6861n9yzzvrcRmsa8xJuF2abe8R+7j++fz3j1C1/I4="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="

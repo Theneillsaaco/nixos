0ac0dbf776bba622597cc06305c3ab5502aa0811
Theneillsaaco <isaacdepena18@gmail.com>
2026-05-03 11:47:26 -0400

    Add CSS hot reload script
    This commit introduces a new script, `css-reload.uc.js`, which enables
    automatic hot reloading of `userChrome.css` and `userContent.css` files.
    This feature is particularly useful for developers and themers who
    frequently modify these files and want to see their changes reflected in
    real-time without manually restarting the browser.
    
    The script monitors the modification times of the CSS files and triggers
    a reload when changes are detected. It also attaches listeners to new
    browser windows and tabs to ensure that the CSS is applied correctly.

--- home/isaac/config/zen/zenBrowser.nix
+++ home/isaac/config/zen/zenBrowser.nix
@@ -1,60 +1,85 @@
-{ pkgs, ... }: 
-
+{  inputs, pkgs, ... }: 
 let
-  caelestiafox = pkgs.writeShellScriptBin "caelestiafox" ''
-    ${pkgs.fish}/bin/fish <<'EOF'
-    function message -a msg
-        set -l x (printf '%08X' (string length -- $msg))
-        printf '%b' "\\x$(string sub -s 7 -l 2 $x)\\x$(string sub -s 5 -l 2 $x)\\x$(string sub -s 3 -l 2 $x)\\x$(string sub -s 1 -l 2 $x)"
-        printf '%s' $msg
-    end
+  zen =
+    inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.beta-unwrapped;
 
-    set -q XDG_STATE_HOME && set -l state $XDG_STATE_HOME || set -l state $HOME/.local/state
-    set -l state_dir $state/caelestia
-    set -l scheme_path $state_dir/scheme.json
+  zen-autoconfig = zen.overrideAttrs (old: rec {
+    libName = "zen-bin";
 
-    if test -f $scheme_path
-        message (jq -c . $scheme_path)
-    else
-        message "{}"
-    end
+    configjs = builtins.fetchurl {
+      url = "https://raw.githubusercontent.com/MrOtherGuy/fx-autoconfig/master/program/config.js";
+      sha256 = "1mx679fbc4d9x4bnqajqx5a95y1lfasvf90pbqkh9sm3ch945p40";
+    };
 
-    inotifywait -q -e 'close_write,moved_to,create' -m $state_dir | while read dir events file
-        test "$dir$file" = $scheme_path && message (jq -c . $scheme_path)
-    end
-    EOF
-  '';
+    configprefs = builtins.fetchurl {
+      url = "https://raw.githubusercontent.com/MrOtherGuy/fx-autoconfig/master/program/defaults/pref/config-prefs.js";
+      sha256 = "sha256-a/0u0TnRj/UXjg/GKjtAWFQN2+ujrckSwNae23DBfs4=";
+    };
+
+    postInstall = (old.postInstall or "") + ''
+      libDir=$(find $out/lib -maxdepth 1 -type d -name "zen-bin*" | head -n1)
+    
+      chmod -R u+w "$libDir"
+      cp ${configjs} "$libDir/config.js"
+    
+      mkdir -p "$libDir/defaults/pref"
+      cp ${configprefs} "$libDir/defaults/pref/config-prefs.js"
+    '';
+  });
+
 in {
-  programs.zen-browser.enable = true;
- 
-   home.packages = with pkgs; [
-     fish
-     jq
-     inotify-tools
-     caelestiafox
-   ];
- 
-   # Native messaging
-   home.file.".mozilla/native-messaging-hosts/caelestiafox.json".text = ''
-   {
-     "name": "caelestiafox",
-     "description": "Native app for CaelestiaFox extension.",
-     "path": "${caelestiafox}/bin/caelestiafox",
-     "type": "stdio",
-     "allowed_extensions": ["caelestiafox@caelestia.org"]
-   }
-   '';
- 
-   # USERCHROME
-   home.file.".config/zen/c2tfi4s0.Default Profile/chrome/userChrome.css".source = ./userChrome.css;
- 
-   home.activation.createZenChromeDir = ''
-     mkdir -p ~/.config/zen/c2tfi4s0.Default\ Profile/chrome
-   '';
- 
-   # Crear scheme.json
-   home.activation.createCaelestiaState = ''
-     mkdir -p ~/.local/state/caelestia
-     [ -f ~/.local/state/caelestia/scheme.json ] || echo '{}' > ~/.local/state/caelestia/scheme.json
-   '';
+  # ⚠️ NO usar programs.zen-browser
+  home.packages = [
+    zen-autoconfig
+  ];
+
+  # Templates
+  xdg.configFile."caelestia/templates/zenChrome.css".source = ./zenChrome.css;
+  xdg.configFile."caelestia/templates/zenContent.css".source = ./zenContent.css;
+
+  # Sine mods
+  home.file.".config/zen/c2tfi4s0.Default Profile/sine-mods/css-hot-reload/css-reload.uc.js".source = ./css-reload.uc.js;
+
+  home.file.".config/zen/c2tfi4s0.Default Profile/sine-mods/css-hot-reload/mod.json".source = ./mod.json;
+
+  home.file.".config/zen/c2tfi4s0.Default Profile/sine-mods/mods.json".text = ''
+  {
+    "css-hot-reload": {
+      "id": "css-hot-reload",
+      "name": "CSS Hot Reload",
+      "enabled": true,
+      "scripts": {
+        "css-reload.uc.js": {
+          "include": ["chrome://browser/content/browser.xhtml"]
+        }
+      }
+    }
+  }
+  '';
+
+  # Activations
+  home.activation.installSine = ''
+    if [ ! -d ~/.config/zen/c2tfi4s0.Default\ Profile/chrome/JS ]; then
+      git clone https://github.com/CosmoCreeper/Sine /tmp/sine
+  
+      cp -r /tmp/sine/chrome/* \
+        ~/.config/zen/c2tfi4s0.Default\ Profile/chrome/
+    fi
+  '';
+  
+  home.activation = {
+    zenSetup = ''
+      mkdir -p ~/.config/zen/c2tfi4s0.Default\ Profile/chrome
+
+      ln -sf ~/.local/state/caelestia/theme/zenChrome.css \
+        ~/.config/zen/c2tfi4s0.Default\ Profile/chrome/userChrome.css
+
+      ln -sf ~/.local/state/caelestia/theme/zenContent.css \
+        ~/.config/zen/c2tfi4s0.Default\ Profile/chrome/userContent.css
+    '';
+
+    caelestiaState = ''
+      mkdir -p ~/.local/state/caelestia/theme
+    '';
+  };
 }

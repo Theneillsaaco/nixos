4b99562ece5cf23ec21a7a14b98431211b6211bf
Theneillsaaco <isaacdepena18@gmail.com>
2026-05-03 20:58:40 -0400

    Add host authentication for localhost

--- modules/programs/postgres.nix
+++ modules/programs/postgres.nix
@@ -5,6 +5,10 @@
     authentication = pkgs.lib.mkOverride 10 ''
       # TYPE  DATABASE    USER        METHOD
       local   all         all         trust
+
+      host    all         all         127.0.0.1/32      trust
+
+      host    all         all         ::1/128           trust
     '';
   };
 }

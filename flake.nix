e1f3610fd317e8e118ffeaf6a40a56a5220dc9f7
Theneillsaaco <isaacdepena18@gmail.com>
2026-05-17 16:06:59 -0400

    Feat: Change home-manager backup extension

--- flake.nix
+++ flake.nix
@@ -86,7 +86,7 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           
-          home-manager.backupFileExtension = "backup";
+          home-manager.backupFileExtension = "hm-backup";
           
           home-manager.users.${username} = 
             import ./home/isaac.nix;

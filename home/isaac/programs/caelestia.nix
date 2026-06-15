eae1aa5c1597965dcc2e0dbd43852994aa4a1b37
Theneillsaaco <isaacdepena18@gmail.com>
2026-06-15 15:09:59 -0400

    Update Caelestia configuration

--- home/isaac/programs/caelestia.nix
+++ home/isaac/programs/caelestia.nix
@@ -31,26 +31,48 @@
       };
 
       general = {
+        showOverFullscreen = false;
+        mediaGifSpeedAdjustment = 300;
+        sessionGifSpeed = 0.7;
+        
         apps = {
-          terminal = [ "kitty" ];
+          terminal = [ "foot" ];
           audio = [ "pavucontrol" ];
           explorer = [ "dolphin" ];
         };
         
         idle = {
           lockBeforeSleep = true;
-          timeouts = [{ timeout = 300; idleAction = "lock"; }];
+          inhibitWhenAudio = true;
+          timeouts = [
+            { timeout = 300; idleAction = "lock"; }
+            { timeout = 300; idleAction = "dpms off"; returnAction = "dpms on"; }
+          ];
         };
       };
 
       bar = {
         persistent = true;
         showOnHover = true;
+        dragThreshold = 20;
         
         workspaces = {
-          activeIndicator = true;
-          showWindows = true;
           shown = 5;
+          activeIndicator = true;
+          occupiedBg = false;
+          showWindows = true;
+          showWindowsOnSpecialWorkspaces = true;
+          maxWindowIcons = 5;
+          activeTrail = false;
+          perMonitorWorkspaces = true;
+          label = "  ";
+          occupiedLabel = "󰮯";
+          activeLabel = "󰮯";
+          capitalisation = "preserve";
+          
+          # special icons
+          specialWorkspaceIcons = [ { name = "steam"; icon = "sports_esports"; } ];
+          windowIcons = [ { regex = "steam(_app_(default|[0-9]+))?"; icon = "sports_esports"; } ];
         };
         
         status = {
@@ -76,26 +98,176 @@
           compact = true;
           recolour = false;
         };
+        
+        entries = [
+          { id = "logo"; enabled = true; }
+          { id = "workspaces"; enabled = true; }
+          { id = "spacer"; enabled = true; }
+          { id = "activeWindow"; enabled = true; }
+          { id = "spacer"; enabled = true; }
+          { id = "tray"; enabled = true; }
+          { id = "clock"; enabled = true; }
+          { id = "statusIcons"; enabled = true; }
+          { id = "power"; enabled = true; }
+        ];
+        
+        excludedScreens = [];
       };
 
       dashboard = {
         enabled = true;
         showOnHover = true;
+        showDashboard = true;
+        showMedia = true;
+        showPerformance = true;
+        showWeather = true;
         mediaUpdateInterval = 500;
+        resourceUpdateInterval = 1000;
+        dragThreshold = 50;
+        
+        performance = {
+          showBattery = true; showGpu = true; showCpu = true;
+          showMemory = true; showStorage = true; showNetwork = true;
+        };
       };
 
       launcher = {
-        actionPrefix = ">";
+        enable = true;
+        showOnHober = false;
         maxShown = 7;
         maxWallpapers = 9;
+        specialPrefix = "@";
+        actionPrefix = ">";
+        enableDangerousActions = false;
+        dragThreshold = 50;
         vimKeybinds = false;
+        favouriteApps = [];
+        hiddenApps = [];
+        
+        useFuzzy = { 
+          apps = false; 
+          actions = false; 
+          schemes = false; 
+          variants = false; 
+          wallpapers = false; 
+        };    
+      
+        actions = [
+          { 
+            name = "Calculator";
+            icon = "calculate";
+            description = "Do simple math equations (powered by Qalc)";
+            command = ["autocomplete" "calc"];
+            enabled = true; 
+            dangerous = false;
+          }
+          { 
+            name = "Scheme"; 
+            icon = "palette";
+            description = "Change the current colour scheme";
+            command = ["autocomplete" "scheme"];
+            enabled = true; 
+            dangerous = false;
+          }
+          { 
+            name = "Wallpaper"; 
+            icon = "image"; 
+            description = "Change the current wallpaper";
+            command = ["autocomplete" "wallpaper"]; 
+            enabled = true;
+            dangerous = false;
+          }
+          { 
+            name = "Variant"; 
+            icon = "colors"; 
+            description = "Change the current scheme variant";
+            command = ["autocomplete" "variant"]; 
+            enabled = true; 
+            dangerous = false;
+          }
+          { name = "Random";
+            icon = "casino"; 
+            description = "Switch to a random wallpaper";
+            command = ["caelestia" "wallpaper" "-r"]; 
+            enabled = true; 
+            dangerous = false;
+          }
+          { name = "Light";
+            icon = "light_mode"; 
+            description = "Change the scheme to light mode";
+            command = ["setMode" "light"];
+            enabled = true;
+            dangerous = false;
+          }
+          { 
+            name = "Dark";
+            icon = "dark_mode";
+            description = "Change the scheme to dark mode";
+            command = ["setMode" "dark"];
+            enabled = true; 
+            dangerous = false; 
+          }
+          { 
+            name = "Shutdown";
+            icon = "power_settings_new";
+            description = "Shutdown the system";
+            command = ["systemctl" "poweroff"];
+            enabled = true;
+            dangerous = true;
+          }
+          { 
+            name = "Reboot";
+            icon = "cached"; 
+            description = "Reboot the system";
+            command = ["systemctl" "reboot"];
+            enabled = true; 
+            dangerous = true; 
+          }
+          { 
+            name = "Logout";
+            icon = "exit_to_app"; 
+            description = "Log out of the current session"; 
+            command = ["loginctl" "terminate-user" ""]; 
+            enabled = true; 
+            dangerous = true;
+          }
+          { 
+            name = "Lock"; 
+            icon = "lock"; 
+            description = "Lock the current session"; 
+            command = ["loginctl" "lock-session"]; 
+            enabled = true;
+            dangerous = false; 
+          }
+          { 
+            name = "Sleep";
+            icon = "bedtime";
+            description = "Suspend then hibernate";
+            command = ["systemctl" "suspend-then-hibernate"];
+            enabled = true; 
+            dangerous = false;
+          }
+          { 
+            name = "Settings";
+            icon = "settings"; 
+            description = "Configure the shell"; 
+            command = ["caelestia" "shell" "nexus" "open"];
+            enabled = true; 
+            dangerous = false;
+          }
+        ];
       };
 
       notifs = {
         expire = true;
+        fullscreen = "on";
         defaultExpireTimeout = 3000;
+        fullscreenExpireTimeout = 2000;
         clearThreshold = 0.3;
         expandThreshold = 20;
+        actionOnClick = false;
+        groupPrevievNum = 3;
+        openExpanded = false;
       };
 
       osd.hideDelay = 2000;
@@ -112,6 +284,10 @@
       };
       
       session = {
+        enabled = true;
+        dragThreshold = 30;
+        vimKeybinds = false;
+        
         commands = {
           logout = [ "hyprctl" "dispatch" "exit" ];
           shutdown = [ "systemctl" "poweroff" ];

043cdd6b08c50b098219ffc56b4c7f97902e969e
Theneillsaaco <isaacdepena18@gmail.com>
2026-08-31 21:58:06 -0400

    Update shell aliases and configure KDE Wallet PAM integration

--- modules/desktop/plasma.nix
+++ modules/desktop/plasma.nix
@@ -2,16 +2,21 @@
   services.xserver.enable = false;
   services.desktopManager.plasma6.enable = true;
   services.xserver.xkb.layout = "us";
-  
+
   programs.partition-manager.enable = true;
   programs.kdeconnect.enable = true;
-  
+
+  security.pam.services.login.kwallet = with pkgs.kdePackages; {
+    enable = true;
+    package = kdePackages.kwallet-pam;
+  };
+
   environment.systemPackages = with pkgs; [
     kdePackages.kio
     kdePackages.kio-extras
     kdePackages.kate
   ];
-  
+
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
     oxygen
     elisa
